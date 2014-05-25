48344d9ef5b527331a1c45f9474f5f4d1fd8bc92
Liam Goodacre <goodacre.liam@gmail.com>
2014-05-25 13:34:49 +0100

    Lens lib refactoring

--- scripts/lib/lens.js
+++ scripts/lib/lens.js
@@ -1,9 +1,13 @@
 /*
+  Some basic lens functionality with helpers for Mori.
+
   Author: Liam Goodacre
 */
 define(['mori'], function (M) {
   var Lib = {}
 
+  //  Modify
+  //: Lens m a -> (a -> b) -> m a -> m b
   var mod = Lib.mod = function (l) {
     return function (f) {
       return function (m) {
@@ -12,39 +16,26 @@ define(['mori'], function (M) {
     }
   }
 
+  //  The lens constructor
   var Lens = Lib.Lens = function (get, set) {
     var inst = {}
     inst.get = get
     inst.set = set
+    //  Provide a convenience modify method
     inst.mod = mod(inst)
     return inst
   }
 
-  var get = function (k) {
-    return function (m) { return M.get(m, k) }
-  }
-
-  var set = function (k) {
-    return function (v) {
-      return function (m) { return M.assoc(m, k, v) }
-    }
-  }
-
-  var lens = Lib.lens = function (k) {
-    return Lens(get(k), set(k))
-  }
-
+  //  Accessor functions
   Lib.get = function (l) { return l.get }
   Lib.set = function (l) { return l.set }
 
-  var id = function (v) { return v }
+  //  The identity lens
+  var idLens = Lib.id = Lens(
+    function (v) { return v },
+    function (v) { return function () { return v } })
 
-  var constant = function (v) {
-    return function () { return v }
-  }
-
-  var idLens = Lib.id = Lens(id, constant)
-
+  //  Lens composition
   var comp = Lib.comp = function (f, g) {
     return Lens(
       function (m) { return g.get(f.get(m)) },
@@ -56,10 +47,27 @@ define(['mori'], function (M) {
       })
   }
 
+  //  Lens sequencing (folded composition)
   var seq = Lib.seq = function (xs) {
     return xs.reduce(comp, idLens)
   }
 
+
+  /*  Mori specific  */
+
+  //  Construct a Mori lens
+  var lens = Lib.lens = function (k) {
+    return Lens(
+      function (m) { return M.get(m, k) },
+      function (v) {
+        return function (m) { return M.assoc(m, k, v) }
+      })
+  }
+
+  //  A mori lens path
+  //  Given a collection of keys
+  //  Build mori lenses form them
+  //  Then sequence them
   var path = Lib.path = function (xs) {
     return seq(xs.map(lens))
   }
